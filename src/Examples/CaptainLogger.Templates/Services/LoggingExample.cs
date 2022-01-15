66ec7b205a1e9c3960e0b723d58e82fbfc9bb98d
Chekko Milani <Chekko.Milani@kennedysiq.com>
2022-01-15 23:38:22 +0000

    Examples

---
+++ src/Examples/CaptainLogger.Templates/Services/LoggingExample.cs
@@ -0,0 +1,32 @@
+﻿namespace CaptainLogger.Templates.Services;
+
+public class LoggingExample : ILoggingExample
+{
+    private readonly ICaptainLogger _logger;
+
+    public LoggingExample(ICaptainLogger<LoggingExample> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogUserIds(
+        int userId,
+        int departmentId,
+        int clientId)
+    {
+        _logger.InformationLog(userId, departmentId, clientId);
+    }
+
+    public void LogWebRequests(
+        string hostname,
+        int port,
+        int statusCode,
+        string method)
+    {
+        if (statusCode >= 500)
+            _logger.WarningLog(method, hostname, port, statusCode);
+        
+        if (statusCode >= 200 && statusCode < 300)
+            _logger.InformationLog(method, hostname, port, statusCode);
+    }
+}
