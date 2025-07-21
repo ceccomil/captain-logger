164d7009accf39e5f71b7209da6b7b1989c9cfa2
Chekko Milani <milani.francesco@outlook.com>
2025-07-21 16:49:51 +0100

    no generic delegate

--- src/CaptainLogger/CaptainLogger.Contracts/HandlerDelegates.cs
+++ src/CaptainLogger/CaptainLogger.Contracts/HandlerDelegates.cs
@@ -1,15 +1,6 @@
 ﻿namespace CaptainLogger.Contracts;
 
 /// <summary>
-/// Generic asynchronous event handler for intercepting log entries.
-/// </summary>
-/// <typeparam name="TState">The type of the log state object.</typeparam>
-/// <param name="evArgs">Log entry details.</param>
-public delegate Task LogEntryRequestedAsyncHandler<TState>(
-  CaptainLoggerEventArgs<TState> evArgs);
-
-
-/// <summary>
 /// Asynchronous event handler for intercepting log entries with boxed state.
 /// </summary>
 /// <param name="evArgs">Log entry details with state boxed as object.</param>
