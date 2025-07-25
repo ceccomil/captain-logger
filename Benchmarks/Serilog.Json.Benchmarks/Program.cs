8fc1742e0c5c02abdcb0c4771c32c690b099137b
Chekko Milani <milani.francesco@outlook.com>
2025-07-25 12:55:21 +0100

    Benchmarks

---
+++ Benchmarks/Serilog.Json.Benchmarks/Program.cs
@@ -0,0 +1,20 @@
+﻿using LogsBenchmark;
+
+if (Directory.Exists(Constants.LOG_FOLDER))
+{
+  Directory.Delete(Constants.LOG_FOLDER, recursive: true);
+}
+
+Directory.CreateDirectory(Constants.LOG_FOLDER);
+
+#if DEBUG
+
+var benchmark = new LogsServiceBenchmark();
+benchmark.Setup();
+benchmark.LogsInParallel();
+
+#else
+
+BenchmarkDotNet.Running.BenchmarkRunner.Run<LogsServiceBenchmark>();
+
+#endif
