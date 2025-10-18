5266e9d75b5d5ff3ee3cd25cf7d1a1c335072928
ceccomil <40296947+ceccomil@users.noreply.github.com>
2025-10-18 11:33:43 +0100

    .NET8, .NET9 and .NET10.0 (#39)
    * Multi targets
    
    * Action fix
    
    ---------
    
    Co-authored-by: Chekko Milani <milani.francesco@outlook.com>

---
+++ Examples/Net8.VerySimpleUseCase/LoggerReceiver.cs
@@ -0,0 +1,24 @@
+﻿using CaptainLogger;
+using CaptainLogger.Contracts.EventsArgs;
+
+namespace VerySimpleUseCase;
+
+public interface ILoggerReceiver
+{
+
+}
+
+internal sealed class LoggerReceiver : ILoggerReceiver
+{
+  public LoggerReceiver(
+    ILogDispatcher dispatcher)
+  {
+    dispatcher.OnLogEntry += OnLogEntry;
+  }
+
+  private Task OnLogEntry(CaptainLoggerEventArgs<object> evArgs)
+  {
+    var stop = "stop";
+    return Task.CompletedTask;
+  }
+}
